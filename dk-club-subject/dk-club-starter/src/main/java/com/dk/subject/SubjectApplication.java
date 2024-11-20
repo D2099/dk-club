8004d42c62f16c86c8e6b67e707896c78f3f39f1
DEMOKING <209733813@qq.com>
2024-11-20 10:33:06 +0800

    feat:添加项目启动类，并加入配置文件以及多环境的动态切换配置

---
+++ dk-club-subject/dk-club-starter/src/main/java/com/dk/subject/SubjectApplication.java
@@ -0,0 +1,16 @@
+package com.dk.subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * 刷题项目启动类
+ */
+@SpringBootApplication(scanBasePackages = "com.dk")
+public class SubjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectApplication.class, args);
+    }
+}
