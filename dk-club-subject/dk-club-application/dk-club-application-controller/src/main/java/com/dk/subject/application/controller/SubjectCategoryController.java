6b77ca7284890653e4b098c5808f545e8b76f4ce
DEMOKING <209733813@qq.com>
2024-12-11 18:31:37 +0800

    feat: 接口添加注释，测试文档生成插件

--- dk-club-subject/dk-club-application/dk-club-application-controller/src/main/java/com/dk/subject/application/controller/SubjectCategoryController.java
+++ dk-club-subject/dk-club-application/dk-club-application-controller/src/main/java/com/dk/subject/application/controller/SubjectCategoryController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 /**
+ * 类别接口控制器
 * @author lxb
 * @since 2024-12-10
 */
@@ -20,9 +21,9 @@ public class SubjectCategoryController {
     private SubjectCategoryService subjectCategoryService;
 
     /**
-     *
-     * @param id
-     * @return
+     * 通过主键ID查询一个类别
+     * @param id 主键ID
+     * @return 类别对象
      */
     @GetMapping("/selectOne")
     public SubjectCategory getSubjectCategory(@RequestParam("id") Integer id){
@@ -30,25 +31,44 @@ public class SubjectCategoryController {
      return  subjectCategoryOne;
    }
 
+    /**
+     * 查询全部类别
+     * @return 类别对象集合
+     */
     @GetMapping("/listAll")
     public List<SubjectCategory> getAllSubjectCategory(){
         List<SubjectCategory> subjectCategoryList = subjectCategoryService.getAllSubjectCategory();
         return  subjectCategoryList;
     }
 
+    /**
+     * 新增类别
+     * @param subjectCategory com.dk.subject.infra.basic.entity.SubjectCategory
+     * @return 暂无
+     */
     @PostMapping("/add")
     public Object add(@Valid @RequestBody SubjectCategory subjectCategory) {
         subjectCategoryService.add( subjectCategory);
-        return  null;
+        return null;
     }
 
+    /**
+     * 更新类别
+     * @param subjectCategory com.dk.subject.infra.basic.entity.SubjectCategory
+     * @return 暂无
+     */
     @PutMapping("/update")
     public int update(@Valid @RequestBody SubjectCategory subjectCategory) {
         int num = subjectCategoryService.modify( subjectCategory);
         return  num;
     }
 
-
+    /**
+     * 通过主键ID删除类别
+     *
+     * @param ids 主键ID（可以多个英文逗号隔开）
+     * @return 暂无
+     */
     @DeleteMapping(value = "/delete/{ids}")
     public Object remove(@NotBlank(message = "{required}") @PathVariable String ids) {
          subjectCategoryService.remove(ids);
--- dk-club-subject/dk-club-application/dk-club-application-controller/src/main/java/com/dk/subject/application/controller/SubjectController.java
+++ dk-club-subject/dk-club-application/dk-club-application-controller/src/main/java/com/dk/subject/application/controller/SubjectController.java
@@ -4,10 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 测试接口控制器
+ */
 @RestController()
 @RequestMapping("/subject")
 public class SubjectController {
 
+    /**
+     * 测试接口连通性
+     * @return
+     */
     @GetMapping("/test")
     public String test() {
         return "hello word";
