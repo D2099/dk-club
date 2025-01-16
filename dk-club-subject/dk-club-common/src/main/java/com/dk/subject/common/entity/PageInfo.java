a96548d9e44cf33d875fbcaa82c6685f79268c11
DEMOKING <209733813@qq.com>
2025-01-16 11:38:38 +0800

    feat: 新增分页请求实体类

---
+++ dk-club-subject/dk-club-common/src/main/java/com/dk/subject/common/entity/PageInfo.java
@@ -0,0 +1,32 @@
+package com.dk.subject.common.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 分页信息封装类
+ */
+@Data
+public class PageInfo implements Serializable {
+
+    private Integer pageNo = 1;
+
+    private Integer pageSize = 20;
+
+    public Integer getPageNo() {
+        if (pageNo == null || pageNo < 1) {
+            return 1;
+        }
+        return pageNo;
+    }
+
+    public Integer getPageSize() {
+        if (pageSize == null || pageSize < 1) {
+            return 20;
+        }
+        return pageSize;
+    }
+
+
+}
