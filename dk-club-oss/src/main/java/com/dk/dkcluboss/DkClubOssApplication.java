3b08872fca139d416d7fbacccd0a43a9714df5d1
DEMOKING <209733813@qq.com>
2025-02-19 16:15:42 +0800

    feat: 创建OSS服务模块

---
+++ dk-club-oss/src/main/java/com/dk/dkcluboss/DkClubOssApplication.java
@@ -0,0 +1,13 @@
+package com.dk.dkcluboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkClubOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DkClubOssApplication.class, args);
+    }
+
+}
---
+++ dk-club-oss/src/test/java/com/dk/dkcluboss/DkClubOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.dkcluboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkClubOssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
