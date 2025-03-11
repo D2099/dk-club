e4e48d555c1255ef7deb9a290029886ff14c90c6
DEMOKING <209733813@qq.com>
2025-03-11 17:03:56 +0800

    feat: 引入SpringCloudAlibaba，Nacos实现Bean对象动态加载

--- dk-club-oss/src/main/java/com/dk/oss/config/StorageConfig.java
+++ dk-club-oss/src/main/java/com/dk/oss/config/StorageConfig.java
@@ -5,15 +5,18 @@ import com.dk.oss.adapter.StorageAdapter;
 import com.dk.oss.adapter.AliStorageAdapter;
 import com.dk.oss.adapter.MinioStorageAdapter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@RefreshScope
 @Configuration
 public class StorageConfig {
 
     @Value("${storage.service.type}")
     private String serviceType;
 
+    @RefreshScope
     @Bean("storageService")
     public StorageAdapter getStorageService() {
         OssTypeEnum ossTypeEnum = OssTypeEnum.getEnumByCode(serviceType);
--- dk-club-oss/src/main/java/com/dk/oss/controller/OssController.java
+++ dk-club-oss/src/main/java/com/dk/oss/controller/OssController.java
@@ -4,6 +4,8 @@ import com.dk.oss.entity.BucketInfo;
 import com.dk.oss.service.FileService;
 import com.dk.oss.util.MinioUtil;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Collectors;
 
+/**
+ * @author 23247
+ */
+@RefreshScope
 @RestController
 @RequestMapping("/oss")
 public class OssController {
@@ -21,6 +27,9 @@ public class OssController {
     @Resource
     private FileService fileService;
 
+    @Value("${storage.service.type}")
+    private String serviceType;
+
     /**
      * 请求测试
      * @return
@@ -28,6 +37,15 @@ public class OssController {
     @GetMapping("/ossTest")
     public String ossTest(){
         return "测试啊~";
+    }
+
+    /**
+     * Nacos动态配置请求测试
+     * @return
+     */
+    @GetMapping("/nacosTest")
+    public String nacosTest(){
+        return serviceType;
     }
 
     /**
