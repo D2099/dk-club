c3ec796e86b49680a30a077f73be71273b363eac
DEMOKING <209733813@qq.com>
2025-03-14 13:35:12 +0800

    change: 调整鉴权模块打包启动类，修改启动类注释

--- dk-club-auth/dk-club-auth-starter/src/main/java/com/dk/auth/AuthApplication.java
+++ dk-club-auth/dk-club-auth-starter/src/main/java/com/dk/auth/AuthApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 刷题项目启动类
+ * 权限认证服务启动类
  */
 @SpringBootApplication(scanBasePackages = "com.dk.auth")
 public class AuthApplication {
