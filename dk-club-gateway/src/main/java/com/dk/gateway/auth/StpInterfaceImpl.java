39aafaa1daf250add52f594be3cdf59cd7601523
DEMOKING <209733813@qq.com>
2025-03-17 17:25:05 +0800

    feat: 新增用户权限或角色信息获取逻辑

--- dk-club-gateway/src/main/java/com/dk/gateway/auth/StpInterfaceImpl.java
+++ dk-club-gateway/src/main/java/com/dk/gateway/auth/StpInterfaceImpl.java
@@ -1,9 +1,14 @@
 package com.dk.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
+import com.dk.gateway.redis.RedisUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import jakarta.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -12,20 +17,37 @@ import java.util.List;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    private String premissionPrefix = "auth.permission";
+
+    private String rolePrefix = "auth.role";
+
+    @Resource
+    private RedisUtil redisUtil;
+
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
         // 返回此 loginId 拥有的权限列表
-        List<String> premissionList = new ArrayList<>();
-        // premissionList.add("subject:category:add");
-        return premissionList;
+        return getAuth(premissionPrefix, loginId);
     }
 
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
         // 返回此 loginId 拥有的角色列表
-        List<String> roleList = new ArrayList<>();
-        // roleList.add("admin");
-        return roleList;
+        return getAuth(rolePrefix, loginId);
     }
 
+    /**
+     * 获取用户认证权限或角色
+     * @param prefix
+     * @param userId
+     * @return
+     */
+    private List<String> getAuth(String prefix, Object userId) {
+        String buildKey = redisUtil.buildKey(prefix, String.valueOf(userId));
+        String value = redisUtil.get(buildKey);
+        if (StringUtils.isEmpty(value)) {
+            return Collections.emptyList();
+        }
+        return new Gson().fromJson(value, List.class);
+    }
 }
