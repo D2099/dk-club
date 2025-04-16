1baa0488c11bdb7fb2f6cabc4276bd8ead1bd4c1
DEMOKING <209733813@qq.com>
2025-04-16 16:47:37 +0800

    feat: 新增通用工具类

---
+++ dk-club-auth/dk-club-auth-common/src/main/java/com/dk/auth/common/utils/GeneralUtil.java
@@ -0,0 +1,23 @@
+package com.dk.auth.common.utils;
+
+import java.util.Random;
+
+/**
+ * 通用工具类
+ */
+public class GeneralUtil {
+
+    /**
+     * 随机字符
+     * @param place 定义随机数的位数
+     */
+    public static String randomGen(int place) {
+        String base = "qwertyuioplkjhgfdsazxcvbnmQAZWSXEDCRFVTGBYHNUJMIKLOP0123456789";
+        StringBuffer sb = new StringBuffer();
+        Random rd = new Random();
+        for (int i = 0; i < place; i++) {
+            sb.append(base.charAt(rd.nextInt(base.length())));
+        }
+        return sb.toString();
+    }
+}
